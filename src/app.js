0982d00d417f821a11e7c18e051b58d64e47808f
Valéria <valeriamenezes022@gmail.com>
2022-12-08 00:27:26 -0300

    criando um endpoint do tipo post para cadastrar um novo filme no JSON

--- src/app.js
+++ src/app.js
@@ -1,15 +1,47 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 const { readFile } = require('./fsUtils');
 
-const app = express();
+const moviesPath = path.resolve(__dirname, './movies.json');
 
-app.get('./movies/:id', async (req, res) => {
+const app = express();
+app.use(express.json());
+
+app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
     const movies = await readFile();
     const findMovie = movies.find((movie) => movie.id === Number(id));
     res.status(200).json(findMovie);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await readFile();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const movies = await readFile();
+    const { movie, price } = req.body;
+
+    const newMovie = {
+      id: movies.length + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
   } catch (error) {
     res.status(404).send({ message: error.message });
   }
